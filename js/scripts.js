dd672eff2921f7a3e61e72701559df5fb6246405
dianovahello <138247816+dianovahello@users.noreply.github.com>
2023-08-08 01:36:35 -0400

    fic smooth scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -1,105 +1,91 @@
 console.log("sup I'm JavaScript");
 
-// hamburger menu 
+// hamburger menu
 
 function menuToggle() {
+  const element = document.getElementById("myNavtoggle");
 
-    const element = document.getElementById("myNavtoggle");
-
-    if (element.className === 'navtoggle') {
-        element.classList.remove("navtoggle");
-        element.classList.add("navtoggle-responsive")
-
-    } else {
-        element.className = 'navtoggle';
-    }
+  if (element.className === "navtoggle") {
+    element.classList.remove("navtoggle");
+    element.classList.add("navtoggle-responsive");
+  } else {
+    element.className = "navtoggle";
+  }
 }
 
+// Smooth scrolling function
+function smoothScrollTo(target) {
+  var headerOffset = 108;
 
-// Smooth scrolling
+  $("html, body").animate(
+    {
+      scrollTop: target.offset().top - headerOffset,
+    },
+    1000,
+    function () {
+      // Callback after animation
+      var $target = $(target);
+      $target.focus();
+      if ($target.is(":focus")) {
+        return false;
+      } else {
+        $target.attr("tabindex", "-1");
+        $target.focus();
+      }
+    }
+  );
+}
 
-// 1. select all links with #
+// Add click event handler to links with hash references
 $('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .on("click", function (event) {
+    if (
+      location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        event.preventDefault();
+        smoothScrollTo(target);
+      }
+    }
+  });
 
-    // 2. Remove links that don't link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-
-    // 3. Add event
-
-    .click(function (event) {
-
-        // On-page links
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-
-            // Figure out element to scroll to
-
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-
-            // Does a scroll target exist?
-
-            if (target.length) {
-
-                // Only prevent default if animation is actually gonna happen
-                event.preventDefault();
-
-                // Calculate the offset for the header height (108px)
-
-                var headerOffset = 108;
-
-                $('html, body').animate({
-
-                    scrollTop: target.offset().top - headerOffset // Subtract the header height from the target position
-                }, 1000, function () {
-
-                    // Callback after animation
-
-                    // Must change focus!
-
-                    var $target = $(target);
-                    $target.focus();
-                    if ($target.is(":focus")) { // Checking if the target was focused
-                        return false;
-                    } else {
-                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                        $target.focus(); // Set focus again
-                    };
-                });
-            }
-        }
-    });
 
 // Subnav tabs
 
 function openWork(evt, workName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    if (
+      tablinks[i].getAttribute("onclick") ===
+      evt.currentTarget.getAttribute("onclick")
+    ) {
+      tablinks[i].classList.add("active");
+    } else {
+      tablinks[i].classList.remove("active");
     }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        if (tablinks[i].getAttribute("onclick") === evt.currentTarget.getAttribute("onclick")) {
-            tablinks[i].classList.add("active");
-        } else {
-            tablinks[i].classList.remove("active");
-        }
-    }
-    document.getElementById(workName).style.display = "block";
+  }
+  document.getElementById(workName).style.display = "block";
 }
-
 
 // Side nav in project
 
 function sideNavToggle() {
-    const element = document.getElementById("mySideNav");
+  const element = document.getElementById("mySideNav");
 
-    if (element.className === 'sidenav-closed') {
-        element.classList.remove("sidenav-closed");
-        element.classList.add("sidenav-250px");
-
-    } else {
-        element.className = 'sidenav-closed';
-    }
+  if (element.className === "sidenav-closed") {
+    element.classList.remove("sidenav-closed");
+    element.classList.add("sidenav-250px");
+  } else {
+    element.className = "sidenav-closed";
+  }
 }
